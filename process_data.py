a2bd4689106896926cf02ab559e0832f610255f9
Marta Ferrer <marta.ferrer@distalmotion.com>
2021-03-07 13:47:38 +0100

    Fix bug: add else clausure

--- process_data.py
+++ process_data.py
@@ -78,16 +78,6 @@ def save_data(df, database_filename):
     df.to_sql(name = 'DisasterDatabase', con=engine, index=False, if_exists='replace')
 
 
-# messages_filepath = 'data\messages.csv'
-# categories_filepath = 'data\categories.csv'
-# database_filepath = 'data\DisasterResponse.db'
-# df = load_data(messages_filepath, categories_filepath)
-# print('Cleaning data...')
-# df = clean_data(df)
-# print('Saving data...\n    DATABASE: {}'.format(database_filepath))
-# save_data(df, database_filepath)
-# print('Cleaned data saved to database!')
-
 def main():
     if len(sys.argv) == 4:
 
@@ -105,7 +95,7 @@ def main():
 
         print('Cleaned data saved to database!')
 
-    #else:
+    else:
         print('Please provide the filepaths of the messages and categories '
               'datasets as the first and second argument respectively, as ' 
               'well as the filepath of the database to save the cleaned data ' 
